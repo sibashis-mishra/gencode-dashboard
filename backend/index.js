a6f2098e623ab333f6fbdec585a12b780a31277c
sibashis.mishra <sibashis.mishra@kratin.co.in>
2024-08-17 21:29:06 +0530

    Added cors to backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://gencode-dashboard.com']
+  }));
+
 app.use(express.json());
 
 // Connect to MongoDB
