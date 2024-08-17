147b150becd3d07131f56b7437c7b305c452c61c
sibashis.mishra <sibashis.mishra@kratin.co.in>
2024-08-17 23:11:34 +0530

    Updated cors

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: [process.env.LOCAL_FRONTEND_URL, process.env.PROD_FRONTEND_URL]
-  }));
+app.use(cors());
 
 app.use(express.json());
 
