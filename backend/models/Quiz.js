4131566acd08ed1e82c58c40758b4e35d70309cd
sibashis.mishra <sibashis.mishra@kratin.co.in>
2024-08-19 14:53:05 +0530

    Added models

---
+++ backend/models/Quiz.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const quizSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  questions: [{
+    question: String,
+    options: [String],
+    correctAnswer: String, // Optional for quizzes
+    points: Number
+  }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }, // Field to track the last update time
+  updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // Field to track the user who made the update
+});
+
+// Middleware to update the `updatedAt` field before saving
+quizSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+export default mongoose.model('Quiz', quizSchema);
